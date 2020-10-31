43b202798020152a12bcc0d76ef68c236c545019
Arjix <53124886+ArjixGamer@users.noreply.github.com>
2020-11-01 01:57:06 +0200

    Update options.py

--- options.py
+++ options.py
@@ -1,2 +1,4 @@
-TMDB_API_KEY = 'your key goes here'
-TMDB_METADATA_UPDATE_INTERVAL = 259200 # three days in seconds, this will check if the last modified date was more than the seconds specified here
+def api_key():
+  return 'your key goes here'
+def update_in_seconds():
+  return 259200 # three days in seconds, this will check if the last modified date was more than the seconds specified here
