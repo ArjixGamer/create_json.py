e212ced9e9cbe2d184da81ce20038ca3a5e05af0
Arjix <53124886+ArjixGamer@users.noreply.github.com>
2020-11-01 02:09:43 +0200

    Update options.py

--- options.py
+++ options.py
@@ -1,4 +1,4 @@
-TMDB_API_KEY = 'your key goes here'
+TMDB_API_KEY = 'XXXXXXXXXXXXXXXXXXX'
 TMDB_METADATA_UPDATE_INTERVAL = 259200 # three days in seconds, this will check if the last modified date was more than the seconds specified here
 
 jsonPath = './database.json'
